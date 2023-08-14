a0dbd1b350f135064ba2d555ad25e4cad7252ef5
OscarHF NTT <oscar.hernandezferrer@emeal.nttdata.com>
2023-08-14 15:09:40 +0200

    add avatar img + hexagonal graphic skills

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const body = document.querySelector("body")
 const puntero = document.querySelector(".puntero")
 
+puntero.style.opacity = "0"
+
 body.addEventListener("mousemove", (e) => {
+	puntero.style.opacity = "1"
 	puntero.style.top = `${e.pageY - 10}px`
 	puntero.style.left = `${e.pageX}px`
 })
