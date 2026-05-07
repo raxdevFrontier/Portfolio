a0bead66eb10897546e14d0f7574f4d1e56c2657
raxdev <raxdev.frontier@gmail.com>
2026-05-07 17:32:21 +0200

    feat: ajuste se secciones

--- src/sections/hero/index.tsx
+++ src/sections/hero/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Hero: React.FC = () => {
 	return (
-		<div className="hero bg-base-200 min-h-screen">
+		<section className="hero bg-base-200 min-h-screen">
 			<div className="hero-content flex-col lg:flex-row-reverse">
 				<img
 					src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
@@ -10,17 +10,18 @@ export const Hero: React.FC = () => {
 				/>
 				<div>
 					<h1 className="text-5xl font-bold">Box Office News!</h1>
-					<p className="py-6">
-						Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae
-						pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean
-						sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa
-						nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti
+					<p className="text-justify py-6">
+						Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex
+						sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis
+						convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus
+						fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada
+						lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti
 						sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
 					</p>
 					<button className="btn btn-primary">Get Started</button>
 				</div>
 			</div>
-		</div>
+		</section>
 	);
 };
 
--- src/sections/myProjects/index.tsx
+++ src/sections/myProjects/index.tsx
@@ -1,18 +1,45 @@
 import React from 'react';
 import './index.scss';
 import Card from '@/components/card';
+import SectionWrapper from '@/components/sectionWrapper';
 
 const Projects: React.FC = () => {
 	return (
-		<div className="projects__wrapper">
-			<p>My Projects</p>
-			<Card
-				title="Titulo Card"
-				description="Descripció Card"
-				tag="TAG"
-				badges={['badge1', 'badge2']}
-			/>
-		</div>
+		<SectionWrapper showDivider>
+			<h2 className="text-2xl font-bold tracking-tight sm:text-3xl">Mis proyectos</h2>
+			<div className="projects__cards mt-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 items-stretch">
+				<Card
+					title="Titulo Card"
+					description="Descripció Card"
+					tag="TAG"
+					badges={['badge1', 'badge2']}
+				/>
+				<Card
+					title="Titulo Card"
+					description="Descripció Card"
+					tag="TAG"
+					badges={['badge1', 'badge2']}
+				/>
+				<Card
+					title="Titulo Card"
+					description="Descripció Card"
+					tag="TAG"
+					badges={['badge1', 'badge2']}
+				/>
+				<Card
+					title="Titulo Card"
+					description="Descripció Card"
+					tag="TAG"
+					badges={['badge1', 'badge2']}
+				/>
+				<Card
+					title="Titulo Card"
+					description="Descripció Card"
+					tag="TAG"
+					badges={['badge1', 'badge2']}
+				/>
+			</div>
+		</SectionWrapper>
 	);
 };
 
