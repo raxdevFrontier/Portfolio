f40b01b32722a4ab303fe225388be3f839a1f6d7
raxdev <raxdev.frontier@gmail.com>
2026-05-13 17:33:16 +0200

    feat: afinar Hero

--- src/sections/hero/index.tsx
+++ src/sections/hero/index.tsx
@@ -3,20 +3,21 @@ import React from 'react';
 export const Hero: React.FC = () => {
 	return (
 		<section className="hero bg-base-200 min-h-screen">
-			<div className="hero-content flex-col lg:flex-row-reverse">
+			<div className="hero-content flex-col lg:flex-row-reverse gap-20">
 				<img
 					src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-					className="max-w-sm rounded-lg shadow-2xl"
+					className="max-w-lg rounded-lg shadow-2xl"
 				/>
 				<div>
-					<h1 className="text-5xl font-bold">Box Office News!</h1>
+					<h1 className="text-4xl font-bold sm:text-5xl">
+						Hola, soy <span className="text-primary underline">RaxDev</span>
+					</h1>
 					<p className="text-justify py-6">
-						Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex
-						sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis
-						convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus
-						fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada
-						lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti
-						sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+						Mi nombre es Oscar y soy un Desarrollador Frontend especializado en React,
+						TypeScript.
+						<br />
+						Me enfoco en construir experiencias web rápidas, limpias y centradas en el
+						usuario.
 					</p>
 					<button className="btn btn-primary">Get Started</button>
 				</div>
