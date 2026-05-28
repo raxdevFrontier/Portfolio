1df72eaa32b34667954ab15ac739f25d43b760da
raxdev <raxdev.frontier@gmail.com>
2026-05-28 18:01:01 +0200

    fix: corregir nom en el footer

--- src/layout/Footer/index.tsx
+++ src/layout/Footer/index.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 export const Footer: React.FC = () => {
 	return (
 		<footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
 			<aside>
-				<p>Copyright © {new Date().getFullYear()} - All right reserved by RaxDev</p>
+				<p>
+					Copyright © {new Date().getFullYear()} - All right reserved by Oscar Hernandez
+				</p>
 			</aside>
 		</footer>
-	)
-}
+	);
+};
 
-export default Footer
+export default Footer;
