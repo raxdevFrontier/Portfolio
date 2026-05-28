430a800369a09e27b9144f5c0260c619eb677bfd
raxdev <raxdev.frontier@gmail.com>
2026-05-28 19:14:09 +0200

    feat: incloure img perfil al Hero + mides img responsive

--- src/sections/hero/index.tsx
+++ src/sections/hero/index.tsx
@@ -1,7 +1,8 @@
 import React, { useContext } from 'react';
 import { I18nContext } from '@/context/i18n/i18n.context';
 
-const DEFAULT_IMAGE = 'https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp';
+// const DEFAULT_IMAGE = 'https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp';
+const MY_IMG_URL = '/mi_persona.JPG';
 
 export const Hero: React.FC = () => {
 	const { t } = useContext(I18nContext);
@@ -10,15 +11,14 @@ export const Hero: React.FC = () => {
 		<section id="home" className="hero bg-base-200 min-h-screen">
 			<div className="hero-content flex-col lg:flex-row-reverse gap-20">
 				<img
-					className="max-w-lg rounded-lg shadow-2xl"
-					src={DEFAULT_IMAGE}
+					className="max-w-lg rounded-lg shadow-2xl w-[12.5rem] md:w-[19rem] lg:w-[25rem]"
+					src={MY_IMG_URL}
 					alt="imagen de perfil"
 				/>
 				<div>
 					<h1 className="text-4xl font-bold sm:text-5xl">
 						{t.hero.title}
 						<span className="text-primary underline">{t.hero.alias}</span>!
-						{/* ¡Hola, soy <span className="text-primary underline">RaxDev</span>! */}
 					</h1>
 					<p className="text-justify py-6">
 						{t.hero.subtitle1}
