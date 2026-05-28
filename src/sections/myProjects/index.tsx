92182a405c1d525f13adcaa1f431cead1357ea98
raxdev <raxdev.frontier@gmail.com>
2026-05-28 17:58:43 +0200

    feat: aplicar diccionari en el Projects (wip)

--- src/sections/myProjects/index.tsx
+++ src/sections/myProjects/index.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './index.scss';
 import Card from '@/components/card';
 import SectionWrapper from '@/components/sectionWrapper';
+import { I18nContext } from '@/context/i18n/i18n.context';
 
 const Projects: React.FC = () => {
+	const { t } = useContext(I18nContext);
+
 	return (
 		<SectionWrapper id="projects" showDivider>
-			<h2 className="text-2xl font-bold tracking-tight sm:text-3xl">Mis proyectos</h2>
+			<h2 className="text-2xl font-bold tracking-tight sm:text-3xl">{t.projects.title}</h2>
 			<div className="projects__cards mt-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 items-stretch">
 				<Card
 					title="Titulo Card"
